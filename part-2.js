cf28e125b6b508bc0259b13fe0f5baffdda95224
breez33 <41165009+breez33@users.noreply.github.com>
2024-04-28 12:45:59 -0500

    Completed part-2 assignment

--- part-2.js
+++ part-2.js
@@ -4,46 +4,49 @@ var rs = require('readline-sync');
 const getIndex = column => column - 1;
 const randomNumInRange = maxNum => Math.floor(Math.random() * (maxNum +1))
 
-const pause = rs.keyInPause('Press any key to start the game.', {
-  guide: false,
-});
+const main = () => {
+  const pause = rs.keyInPause('Press any key to start the game.', {
+    guide: false,
+  });
 
-const startGame = () => {
-  let gameBoard = initGameBoard(10);
+  let board = initBoard(10);
   let gameOver = false;
 
+  placeShips(board);
+  console.log(board)
+
   while(!gameOver) {
-    gameBoard = updateGameBoard(gameBoard, [...getNextMove(gameBoard)]);
-    if(getShipsRemaining(gameBoard) === 0) gameOver = true;
+    updateBoard(board, [...getNextMove(board)]);
+    if(calcShipsRemaining(board) === 0) gameOver = true;
+    console.log(board);
   }
 
   // Restart game Y/N
-  if(rs.keyInYN('You have destroyed all battleships. Would you like to play again?') === true) return startGame();
+  if(rs.keyInYN('You have destroyed all battleships. Would you like to play again?') === true) return main();
 }
 
 // Game start
-startGame();
+main();
 
 // Helper functions
-function initGameBoard(gridSize) {
+function initBoard(gridSize) {
   let board = {};
 
   for (let i = 0; i < gridSize; i++) {
     board[String.fromCharCode(65 + i)] = '0'.repeat(gridSize).split('').map(Number);
   }
 
-  initShips(board);
   return board
 }
 
-function initShips(board) {
-  const shipLengths = [5, 4, 3, 3, 2]
+function placeShips(board) {
+  const shipLengths = [5, 4, 3, 3.1, 2]
   
   for (let length of shipLengths) {
-      let shipCoords = generateRandomCoords(board, length);
-      for ([row, col] of shipCoords) {
-          board[row][col] = length
-      }
+    let shipCoords = generateRandomCoords(board, length);
+    for ([row, col] of shipCoords) {
+        board[row][col] = length
+    }
   }
 }
 
@@ -55,16 +58,16 @@ function generateRandomCoords(board, length) {
   
   const direction = directionValues[randomNumInRange(1)]
   const axis = axisValues[randomNumInRange(1)]
-  const startRow = String.fromCharCode(randomNumInRange(gridSize - length) + 65)
+  const startRow = String.fromCharCode(randomNumInRange(gridSize) + 65)
   const startCol = randomNumInRange(gridSize);
   const coordsArray = [[startRow, startCol]]
   
   if (axis === 'x') {
-      for (let i = 1; i < length; i++) {
+      for (let i = 1; i < Math.floor(length); i++) {
           coordsArray.push([startRow, startCol + (i * direction)]);
       }
   } else if (axis === 'y'){
-      for (let i = 1; i < length; i++) {
+      for (let i = 1; i < Math.floor(length); i++) {
           const nextRow = String.fromCharCode(startRow.codePointAt(0) + i)
           coordsArray.push([nextRow, startCol]);
       }
@@ -93,45 +96,58 @@ function isValidCoords(coords, board) {
 function getNextMove(board) {
   const regexString = `^[A-${String.fromCharCode(Math.floor(65 + board['A'].length))}][1-9]|10$`
   const regex = new RegExp(regexString)
-  console.log(regex);
+
   const nextMove = rs.question("Enter a location to strike ie 'A2': ", {
     limit: regex,
     limitMessage: "Invalid coordinates. Please use this format: 'A2'"
   })
-  const [row, col] = [...nextMove]
+  const [row, col] = nextMove.split(/^([a-zA-Z])(\d+)/).slice(1)
+  console.log([row, col])
   const boardAtCoords = board[row][getIndex(col)]
 
   // Location has already been chosen (2 or 3)
-  if (boardAtCoords === 2 || boardAtCoords === 3) {
+  if (boardAtCoords === 1 || boardAtCoords === 9) {
     console.log('You have already picked this location. Miss!')
     return getNextMove(board);
   }
 
-  return [...nextMove];
+  return [row, col];
 }
 
-function updateGameBoard(board, [row, col]) {
-  let boardAtCoords = board[row][getIndex(col)]
+function updateBoard(board, [row, col]) {
+  const valueAtCoords = board[row][getIndex(col)]
 
-  if (boardAtCoords === 0) {            // Location is empty (0): Miss
-    board[row][getIndex(col)] = 3
+  if (valueAtCoords === 0) {            // Location is empty (0): Miss
+    board[row][getIndex(col)] = 1
     console.log('You have missed!')
-  } else if (boardAtCoords === 1) {    // Location has a ship that hasn't already been selected (1): Hit
-    board[row][getIndex(col)] = 2
-    console.log(`Hit. You have sunk a battleship. ${getShipsRemaining(board) > 1 ? `${getShipsRemaining(board)} ships` : `1 ship`} remaining.`)
+  } else if (valueAtCoords > 1) {    // Location has a ship that hasn't already been selected (1-5): Hit
+    board[row][getIndex(col)] = 9
+    console.log("value at coords: " + valueAtCoords)
+
+    const hitConfirmationStr = `${isShipSunk(board, valueAtCoords) ? 'Hit. You have sunk a battleship.' : 'Hit.'}`
+    const shipsRemainingStr = `${calcShipsRemaining(board) > 1 ? `${calcShipsRemaining(board)} ships` : `1 ship`} remaining.`
+    console.log(`${hitConfirmationStr} ${shipsRemainingStr}`)
   }
   
   return board;
 }
 
-function getShipsRemaining(board) {
-  return Object.entries(board).reduce((shipCount, [_, col]) => {
-    let count = 0
-
-    for (let elem of col) {
-      if(elem === 1) count++
+function calcShipsRemaining(board) {
+  const shipValues = [5, 4, 3, 3.1, 2];
+  return shipValues.reduce((shipsRemaining, ship) => {
+    for (let [_, col] of Object.entries(board)) {
+      // if(col.find(value => value === parseInt(ship))) return shipsRemaining += 1;
+      if (col.find(value => value === ship)) return shipsRemaining += 1;
     }
-
-    return shipCount += count;
+    
+    return shipsRemaining;
   }, 0);
+}
+
+function isShipSunk(board, ship) {
+  for (let [_, col] of Object.entries(board)) {
+    if(col.find(value => value === ship)) return false;
+  }
+
+  return true;
 }
